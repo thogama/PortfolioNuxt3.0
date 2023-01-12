4cb1c69961a707346e773df946aeeffe535a14b2
Alan Jonatas De Oliveira Gama <202000012444@office.ufs.br>
2023-01-12 14:42:37 -0300

    first idea

--- components/FirstSection.vue
+++ components/FirstSection.vue
@@ -1,3 +1,24 @@
+<script lang="ts" setup>
+
+let state = useState("range", () => 0)
+
+let a = () => { console.log(state.value) }
+
+</script>
 <template>
-    
+    <div class="container">
+        <img @click="a()" class="shape img-fluid" src="~/assets/IA-59 (1).jpg" />
+        <label for="customRange1" class="form-label">Example range</label>
+        <input v-model="state" type="range" class="form-range" id="customRange1">
+
+    </div>
+
 </template>
+
+
+<style>
+.shape {
+    clip-path: circle(50% at 50% 50%);
+
+}
+</style>
