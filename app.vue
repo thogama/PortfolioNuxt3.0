6e3c55e8ba16fe2cd7ea8c27416b3f42178a9e26
Alan Jonatas de Oliveira Gama <alan.gama@pmsc.local>
2023-01-12 11:47:14 -0300

    logo added on assets

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap');
+
+html {
+  font-family: 'Nova Mono', monospace;
+  margin: 0;
+}
+</style>
