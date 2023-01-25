0cb38ea2672fdd52842b7edd9d7f512715acc72c
Alan Jonatas De Oliveira Gama <202000012444@office.ufs.br>
2023-01-25 14:42:57 -0300

    Carousel start

--- components/LeftSide.vue
+++ components/LeftSide.vue
@@ -2,18 +2,46 @@
     <LeftContainer>
         <div class="px-3 bg-primary shadow-lg sticky-top">
             <NavbarL />
+
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div v-for="img,index in projectsImg" class="carousel-item active" data-bs-interval="1000">
+                        <k-container>
+                            <k-element>
+                                <img :src=img class="d-block img-fluid col-4 ">
+                                <div class="fs-3">
+                                    teste
+                                </div>
+                            </k-element>
+                        </k-container>
+
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     </LeftContainer>
 </template>
 
-<script>
-export default {
-    setup() {
+<script setup>
+let projectsImg = [
+    "logo.png",
+    "logo.png",
+    "logo.png"
+]
+projectsImg = projectsImg.map(element => "/_nuxt/assets/" + element)
 
+console.log(projectsImg)
 
-        return {}
-    }
-}
 </script>
 
 <style lang="scss" scoped>
