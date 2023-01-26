f9bc655ae561b3ebba8f28b827d275300959ebc6
Alan Jonatas De Oliveira Gama <202000012444@office.ufs.br>
2023-01-26 14:40:31 -0300

    usando refs?

--- components/LeftSide.vue
+++ components/LeftSide.vue
@@ -5,28 +5,48 @@
 
             <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
-                    <div v-for="img,index in projectsImg" class="carousel-item active" data-bs-interval="1000">
-                        <k-container>
-                            <k-element>
-                                <img :src=img class="d-block img-fluid col-4 ">
-                                <div class="fs-3">
-                                    teste
-                                </div>
-                            </k-element>
-                        </k-container>
+                    <div v-for="img, index in projectsImg">
+                        <div v-if="index == 0" class="carousel-item active" data-bs-interval="10000">
+                            <k-container>
+                                <k-element>
+                                    <img :src=img class="d-block img-fluid col-4  ">
+                                    <div class="fs-3">
+                                        teste
+                                    </div>
+                                </k-element>
+                            </k-container>
+                        </div>
+                        <div v-else class="carousel-item " data-bs-interval="10000">
+                            <k-container>
+                                <k-element>
+                                    <img :src=img class="d-block img-fluid col-4 ">
+                                    <div class="fs-3">
+                                        teste2
+                                    </div>
+                                </k-element>
+                            </k-container>
+
+                        </div>
+
+
 
                     </div>
+
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
                     data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                <button ref="clickOnload" class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
                     data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
+                <div class="progress" style="height: 2px;">
+                <div class="progress-bar bg-success slideTimer" role="progressbar"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
             </div>
         </div>
     </LeftContainer>
@@ -40,10 +60,21 @@ let projectsImg = [
 ]
 projectsImg = projectsImg.map(element => "/_nuxt/assets/" + element)
 
-console.log(projectsImg)
+let click = ref("clickOnload")
 
+console.log(click)
 </script>
 
 <style lang="scss" scoped>
+@keyframes slideTime {
+    0% {
+        width: 0%;
+    }
 
+    100% {width: 100%;}
+}
+
+.slideTimer{
+    animation: slideTime 20s infinite;
+}
 </style>
