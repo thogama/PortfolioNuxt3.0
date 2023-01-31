c9ae643c74e0557adb2319de2aef641a0986ae35
Alan Jonatas De Oliveira Gama <202000012444@office.ufs.br>
2023-01-31 14:40:40 -0300

    continue qualification

--- components/Professional.vue
+++ components/Professional.vue
@@ -1,18 +1,36 @@
 <template >
-    <div class="text-center col-sm-6 justify-content-center d-flex flex-column">
+    <div class=" my-2 text-center col-sm-6 justify-content-center d-flex flex-column">
 
-        <div class="card">
+        <div class="card ">
             <div class="card-header  fs-4  ">
                 Professional
             </div>
 
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body border bg-dark text-white rounded-bottom">
+                <div v-for="link in links" class="d-flex justify-content-around align-items-center my-4">
+                    <Icon class="fs-1" :href=link.href :name=link.icon />
+                    <div class="text-white">
+                        {{ link.name }}
+                    </div>
+                </div>
             </div>
         </div>
 
     </div>
 
 </template>
+<script lang="ts" setup>
+let links: Array<{ name: string, icon: string, href: string }> = [
+    {
+        name: "Linkedin",
+        icon: "logos:linkedin-icon",
+        href: "https://www.linkedin.com/in/alan-gama-devs/"
+    },
+    {
+        name: "Upwork",
+        icon: "cib:upwork",
+        href: ""
+    },
+]
+
+</script>
