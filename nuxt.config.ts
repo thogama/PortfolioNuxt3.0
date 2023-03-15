981ec1354e33fdc0c0830b231f5d5806fe1dea4f
thogama <dethfiregrasp@gmail.com>
2023-03-14 22:10:20 -0300

    Internalize test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@
 export default defineNuxtConfig({
     ssr: false,
     css: ["~~/sass/main.scss"],
-    modules: ['nuxt-icon'],
+    modules: ['nuxt-icon', '@nuxtjs/i18n'],
+    i18n: {
+        detectBrowserLanguage: {
+            alwaysRedirect: true
+        },
+        locales: [
+            { code: 'en', name: 'English', iso: 'en-US', file: 'en-US.json' },
+            { code: 'pt', name: 'Português', iso: 'pt-BR', file: 'pt-BR.json' }
+        ],
+        lazy: true,
+        langDir: 'locales/',
+        vueI18n: {
+            fallbackLocale: 'en'
+        },
+        vueI18nLoader: true
+
+    },
     nitro: {
         preset: "firebase"
     }
